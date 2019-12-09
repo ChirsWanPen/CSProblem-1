384a952e6dd17065c5f41921512b80fc26ec075d
ChirsWanPen <57445880+ChirsWanPen@users.noreply.github.com>
2019-12-09 17:58:16 +0800

    基本完成2

--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
 package CSproblem.work;
+import java.util.*;
 
 public class Main {
 	public static void main ( String args[]){
 		Win a = new Win();
+		/*---------Test----------*/
+		/*
+		Scanner cin = new Scanner(System.in);
+		BinaryTree t = new BinaryTree();
+		t.source = cin.next();
+		t.insuffixConvertToSuffix();
+		t.buildTree();
+		t.cnt = 0; t.prefix(t.rt, t.expAnswer);
+		for(int i = 0; i < t.cnt; i++) t.prefixStr += t.expAnswer[i]+" ";
+		t.cnt = 0; t.infix(t.rt, t.expAnswer);
+		for(int i = 0; i < t.cnt; i++) t.infixStr += t.expAnswer[i]+" ";
+		t.cnt = 0; t.suffix(t.rt, t.expAnswer);
+		for(int i = 0; i < t.cnt; i++) t.suffixStr += t.expAnswer[i]+" ";
+		t.calcAnswer();
+		System.out.println(t.prefixStr);
+		System.out.println(t.infixStr);
+		System.out.println(t.suffixStr);
+		System.out.println("Hello :" + t.ansNum);
+		*/
 	}
 }
